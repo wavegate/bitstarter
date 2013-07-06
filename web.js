dd7490fd83c02efab5abad7b0052442385626cea
Ubuntu <cc.frankee@gmail.com>
2013-07-06 17:40:32 +0000

    Another try at web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var read1 = fs.readFileSync('index.html');
+
+var read2 = read1.toString();
+
 app.get('/', function(request, response) {
-  response.send("Hello World!");
+  response.send(read2);
 });
 
 var port = process.env.PORT || 5000;
