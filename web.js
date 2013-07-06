1efcf2b7d817e266fb670449dc6402329dc38421
Ubuntu <cc.frankee@gmail.com>
2013-07-06 17:26:08 +0000

    Undo web.js

--- web.js
+++ web.js
@@ -2,12 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var reading = fs.readFileSync("index.html");
-
-var reading2 = reading.toString();
-
 app.get('/', function(request, response) {
-  response.send(reading2);
+  response.send("Hello World!");
 });
 
 var port = process.env.PORT || 5000;
